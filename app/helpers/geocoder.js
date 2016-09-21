67ed5c9bd1252857d9cb5f4a7e6bdc260ae0db4f
Jurvis Tan <jurvis.tan@me.com>
2016-09-21 21:20:13 +0800

    add geocoder

---
+++ app/helpers/geocoder.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+const GOOGLE_GEOCODING_API_KEY = 'AIzaSyDQfzJijMkV_sX8Slnyv0A7J7Bsq6jAQ_o';
+
+module.exports = {
+  getLatLng: function(cityName) {
+    var params = {
+      address: cityName,
+      key: GOOGLE_GEOCODING_API_KEY,
+    }
+
+    return fetch('https://maps.googleapis.com/maps/api/geocode/json?' + this.makeParamsQueryString(params))
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(data) {
+        return data.results[0].geometry.location;
+      })
+      .catch(function(error) {
+        console.log('geocoder request failed', error)
+      });
+  },
+
+  makeParamsQueryString: function(params) {
+    var esc = encodeURIComponent;
+    return Object.keys(params)
+      .map(k => esc(k) + '=' + esc(params[k]))
+      .join('&');
+  }
+}
