26d6fbda4eb5118a581dfd351c2993e9743a7c97
Jurvis Tan <jurvis.tan@me.com>
2016-09-27 22:25:48 -0700

    update hk url

--- app/constants/urls.js
+++ app/constants/urls.js
@@ -5,7 +5,7 @@ module.exports = {
   SINGAPORE_MANUAL_URL: "http://www.haze.gov.sg/haze-updates/pollutant-concentrations/type/PM25-1Hr#pollutant",
   SINGAPORE_BACKUP_URL: "http://dev.neaaws.com/psi.xml",
   MALAYSIA_URL: "http://apims.doe.gov.my/v2/hour%d_%s.html",
-  HONGKONG_URL:"http://www.aqhi.gov.hk/en/aqhi/pollutant-and-aqhi-distribution.html",
+  HONGKONG_URL:"http://www.aqhi.gov.hk/epd/ddata/html/out/24pc_Eng.xml",
   THAILAND_URL: "http://www.aqmthai.com/index.php?lang=en",
   TAIWAN_URL: "http://opendata.epa.gov.tw/ws/Data/REWXQA/?$orderby=SiteName&$skip=0&format=json"
 }
