015911bce9cfde3d8c6d0c074a565bf844fdcb6f
Jurvis Tan <jurvis.tan@me.com>
2017-06-09 23:12:24 +0800

    remove dotenv requirement in urls.js

--- app/constants/urls.js
+++ app/constants/urls.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 module.exports = {
   SINGAPORE_URL: `http://api.nea.gov.sg/api/WebAPI?dataset=pm2.5_update&keyref=${process.env.NEA_API_KEY}`,
   SINGAPORE_MANUAL_URL: "http://www.haze.gov.sg/haze-updates/pollutant-concentrations/type/PM25-1Hr#pollutant",
