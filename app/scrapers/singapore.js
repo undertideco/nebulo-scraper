f4768cacba5460dd0a9eb770a76d42d10be8cff0
Duncan Leo <duncanleo97@gmail.com>
2018-01-21 12:37:32 +0800

    modernise Singapore code

--- app/scrapers/singapore.js
+++ app/scrapers/singapore.js
@@ -1,40 +1,29 @@
 const fetch = require('node-fetch');
 const urls = require('../constants/urls');
-const parseString = require('xml2js').parseString;
+const Promise = require('bluebird');
+const xml2js = require('xml2js');
+
+const parseString = Promise.promisify(xml2js.parseString);
 
 const codeMap = {
-  'rNO': 'North, Singapore',
-  'rCE': 'Central, Singapore',
-  'rEA': 'East, Singapore',
-  'rWE': 'West, Singapore',
-  'rSO': 'South, Singapore'
-}
+  rNO: 'North, Singapore',
+  rCE: 'Central, Singapore',
+  rEA: 'East, Singapore',
+  rWE: 'West, Singapore',
+  rSO: 'South, Singapore',
+};
 
 module.exports = {
-  scrape: () => {
-    console.log("Beginning Singapore Scrape...");
-    return fetch(urls.SINGAPORE_URL)
-      .then((response) => {
-        return response.text();
-      })
-      .then((xml) => {
-        var regions;
-        parseString(xml, function (err, result) {
-          regions = result.channel.item[0].region
-        });
-        return regions;
-      })
-      .then((regions) => {
-        return regions.map((region) => {
-          return {
-            name: codeMap[region.id[0]],
-            location: {
-              lat: region.latitude[0],
-              lng: region.longitude[0],
-            },
-            data: parseInt(region.record[0].reading[0].$.value, 10)
-          }
-        });
-      });
-  },
-}
+  scrape: () => fetch(urls.SINGAPORE_URL)
+    .then(response => response.text())
+    .then(parseString)
+    .then(result => result.channel.item[0].region)
+    .then(regions => regions.map(region => ({
+      name: codeMap[region.id[0]],
+      location: {
+        lat: region.latitude[0],
+        lng: region.longitude[0],
+      },
+      data: parseInt(region.record[0].reading[0].$.value, 10),
+    }))),
+};
