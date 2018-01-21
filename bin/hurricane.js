6c78b19081074b6984db3067f6f99b5f268ef450
Duncan Leo <duncanleo97@gmail.com>
2018-01-21 11:31:07 +0800

    Added missing call to USA scraper

--- bin/hurricane.js
+++ bin/hurricane.js
@@ -5,6 +5,7 @@ const sg = require('../app/scrapers/singapore');
 const my = require('../app/scrapers/malaysia');
 const tw = require('../app/scrapers/taiwan');
 const hk = require('../app/scrapers/hongkong');
+const usa = require('../app/scrapers/usa');
 const nl = require('../app/scrapers/netherlands');
 
 sg.scrape().then(function(result){
@@ -36,6 +37,13 @@ hk.scrape().then(function(result) {
 })
 
 nl.scrape().then(function(result) {
+  result.forEach(function(city) {
+    console.log(city);
+    db.dispatchCity(city);
+  });
+})
+
+usa.scrape().then(function(result) {
   result.forEach(function(city) {
     console.log(city);
     db.dispatchCity(city);
