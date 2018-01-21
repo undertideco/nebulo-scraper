96eb218e67a2f1389dab9e4cbe97ea016930578a
Duncan Leo <duncanleo97@gmail.com>
2018-01-21 12:13:03 +0800

    modernise USA code

--- app/scrapers/usa.js
+++ app/scrapers/usa.js
@@ -4,37 +4,23 @@ const urls = require('../constants/urls');
 const geocoder = require('../helpers/geocoder');
 
 module.exports = {
-  scrape: () => {
-    return fetch(urls.USA_URL)
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        return json.map((city) => {
-          return  {
-            name: null,
-            data: parseInt( city['AQI'], 10) || 0,
-            location: {
-              lat: city.Latitude,
-              lng: city.Longitude
-            }
-          }
-        });
-      })
-      .then((citiesData) => {
-        return Promise.map(citiesData, (city) => {
-          return geocoder.getAddress(city.location.lat, city.location.lng)
-            .then(function(address) {
-              city.name = address;
-              return city;
-            });
-        }, { concurrency: 2 })
-          .then(function(results) {
-            return results;
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
-      });
-  }
-}
+  scrape: () => fetch(urls.USA_URL)
+    .then(response => response.json())
+    .then(json => json.map(city => ({
+      name: null,
+      data: parseInt(city.AQI, 10) || 0,
+      location: {
+        lat: city.Latitude,
+        lng: city.Longitude,
+      },
+    })))
+    .then(citiesData => Promise.map(
+      citiesData,
+      city => geocoder.getAddress(city.location.lat, city.location.lng)
+        .then(address => Object.assign(
+          city,
+          { name: address },
+        )),
+      { concurrency: 2 },
+    ).catch(console.error)),
+};
