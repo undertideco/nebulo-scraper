baa25d3bbe6e7bc44a24b7577ffd02c86f21c9ad
Duncan Leo <hello@duncanleo.me>
2019-02-16 22:10:05 +0800

    use async/await for USA

--- app/scrapers/usa.js
+++ app/scrapers/usa.js
@@ -4,23 +4,25 @@ const urls = require('../constants/urls');
 const geocoder = require('../helpers/geocoder');
 
 module.exports = {
-  scrape: () => fetch(urls.USA_URL)
-    .then(response => response.json())
-    .then(json => json.map(city => ({
+  scrape: async () => {
+    const json = await fetch(urls.USA_URL)
+      .then(response => response.json());
+    const cities = json.map(city => ({
       name: null,
       data: parseInt(city.AQI, 10) || 0,
       location: {
         lat: city.Latitude,
         lng: city.Longitude,
       },
-    })))
-    .then(citiesData => Promise.map(
-      citiesData,
+    }));
+    return Promise.map(
+      cities,
       city => geocoder.getAddress(city.location.lat, city.location.lng)
         .then(address => Object.assign(
           city,
           { name: address },
         )),
       { concurrency: 2 },
-    ).catch(console.error)),
+    );
+  },
 };
