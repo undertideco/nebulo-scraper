9c38c5bb11f7f01906d405c65fa76e28e72a2536
Duncan Leo <hello@duncanleo.me>
2019-02-16 22:02:53 +0800

    use async/await for Malaysia

--- app/scrapers/malaysia.js
+++ app/scrapers/malaysia.js
@@ -6,31 +6,33 @@ const urls = require('../constants/urls');
 const numberRegex = /(\d+)/;
 
 module.exports = {
-  scrape: () => fetch(urls.MALAYSIA_URL)
-    .then(response => response.json())
-    .then((json) => {
-      const cities = json['24hour_api'];
-      cities.shift();
+  scrape: async () => {
+    const json = await fetch(urls.MALAYSIA_URL)
+      .then(response => response.json());
 
-      return cities.map((city) => {
-        const obj = {
-          name: `${city[1]}, ${city[0].charAt(0) + city[0].substring(1).toLowerCase()}`,
-          data: 0,
-        };
+    const cities = json['24hour_api'];
+    cities.shift();
 
-        const data = city.slice(-1)[0];
+    const citiesProcessed = cities.map((city) => {
+      const obj = {
+        name: `${city[1]}, ${city[0].charAt(0) + city[0].substring(1).toLowerCase()}`,
+        data: 0,
+      };
 
-        if (numberRegex.test(data)) {
-          obj.data = parseInt(data.match(numberRegex)[0], 10);
-        }
+      const data = city.slice(-1)[0];
 
-        return obj;
-      });
-    })
-    .then(citiesData => Promise.map(
-      citiesData,
+      if (numberRegex.test(data)) {
+        obj.data = parseInt(data.match(numberRegex)[0], 10);
+      }
+
+      return obj;
+    });
+
+    return Promise.map(
+      citiesProcessed,
       city => geocoder.getLatLng(city.name)
         .then(location => Object.assign(city, { location })),
       { concurrency: 2 },
-    ).catch(console.error)),
+    );
+  },
 };
