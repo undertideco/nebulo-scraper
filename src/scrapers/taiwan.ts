3853d2b5e08370829cccc94c96cb163ff175b2ea
Duncan Leo <hello@duncanleo.me>
2026-03-01 23:32:05 +0800

    fix(taiwan): update API

--- src/scrapers/taiwan.ts
+++ src/scrapers/taiwan.ts
@@ -16,38 +16,6 @@ const geocoderExceptions: Record<string, string> = {
   '安南, 臺南市': '安南',
 };
 
-interface Root {
-  fields: Field[];
-  resource_id: string;
-  __extras: Extras;
-  include_total: boolean;
-  total: string;
-  resource_format: string;
-  limit: string;
-  offset: string;
-  _links: Links;
-  records: PM25Record[];
-}
-
-interface Field {
-  id: string;
-  type: string;
-  info: Info;
-}
-
-interface Info {
-  label: string;
-}
-
-interface Extras {
-  api_key: string;
-}
-
-interface Links {
-  start: string;
-  next: string;
-}
-
 interface PM25Record {
   site: string;
   county: string;
@@ -57,7 +25,7 @@ interface PM25Record {
 }
 
 export default async function taiwan(): Promise<App.City[]> {
-  const result = await axios.get<Root>(TAIWAN_URL.toString(), {
+  const result = await axios.get<PM25Record[]>(TAIWAN_URL.toString(), {
     headers: {
       'User-Agent': USER_AGENT,
       Accept: 'application/json',
@@ -65,7 +33,7 @@ export default async function taiwan(): Promise<App.City[]> {
   });
 
   return Bluebird.map(
-    result.data.records,
+    result.data,
     async (city): Promise<App.City> => {
       const name = `${city.site}, ${city.county}`;
       const data = parseInt(city.pm25, 10) || 0;
